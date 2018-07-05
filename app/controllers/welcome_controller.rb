4388a5182b899f4cf41ba3314104f058d2fe7b48
Kenneth Berland <berlandk@amazon.com>
2018-07-04 19:42:14 -0700

    add encrypt for emails

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,13 @@
+require 'openssl'
+require 'base64'
+
+
+KEY = [Digest::SHA1.hexdigest('83V3jg%@FOcjskHb#')[0..32]].pack("H*")
+
 class WelcomeController < ApplicationController
   def index
     @email = params['email']
+    @decrypted_email = decrypt(KEY, Base64.urlsafe_decode64(params['email']))
     @service = params['service']
     @lunch = params['lunch']
     @party = params['party']
@@ -9,11 +16,28 @@ class WelcomeController < ApplicationController
     if params['commit']
       st = ActiveRecord::Base.connection.raw_connection.prepare(
         "replace into rsvps (email, event, heads, updated_at) values (?, ?, ?, now()), (?, ?, ?, now())")
-      st.execute(@email, 'service', @service.to_i,
-                 @email, 'party', @party.to_i)
+      st.execute(@decrypted_email, 'service', @service.to_i,
+                 @decrypted_email, 'party', @party.to_i)
       st.close
     end
   end
 
-  private 
+  private
+
+  def aes(m,k,t)
+    (aes = OpenSSL::Cipher::Cipher.new('aes-256-cbc').send(m)).key = Digest::SHA256.digest(k)
+    aes.update(t) << aes.final
+  end
+
+  def encrypt(key, text)
+    aes(:encrypt, key, text)
+  end
+
+  def decrypt(key, text)
+    aes(:decrypt, key, text)
+  end
+
+  def bin_to_hex(str)
+    str.unpack('C*').map{ |b| "%02X" % b }.join('')
+  end
 end
